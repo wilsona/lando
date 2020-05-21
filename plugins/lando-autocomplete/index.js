147e42d57bbc00248d61be2305c71b7cbab1ff0a
Andrew Wilson <awilson8@uw.edu>
2020-05-20 22:39:41 -0700

    WIP: using options as suggestions only when argument not expected

--- plugins/lando-autocomplete/index.js
+++ plugins/lando-autocomplete/index.js
@@ -4,15 +4,35 @@ const _ = require('lodash');
 
 const getCommandArgumentCompletion = (task, optionsInput) => {
     let options = [];
-    options = Object.keys(task.options);
+    let lastInput = _.last(optionsInput);
+    let lastOption;
 
-    const missingCompletion = !options instanceof Array;
-    if (missingCompletion) {
-        console.log('Missing completion');
-        return;
+    if (!_.startsWith(lastInput, '--')) {
+        // we have an empty string or a partial option argument string
+        // get the previous input
     }
 
-    _.pullAll(options, optionsInput);
+    if (_.startsWith(lastInput, '--')) {
+        let optionName = _.trimStart(lastInput, '-');
+        let lastOption = task.options[optionName];
+        let lastOptionBoolean = option.boolean;
+    }
+
+    if (!lastInput || lastOptionBoolean) {
+        // suggest a new option
+        options = Object.keys(task.options);
+
+        const missingCompletion = !options instanceof Array;
+        if (missingCompletion) {
+            console.log('Missing completion');
+            return;
+        }
+
+        options = _.map(options, option => `--${option}`);
+        _.pullAll(options, optionsInput);
+    } else {
+        // try to complete an argument to the last option
+    }
 
     return options;
 };
